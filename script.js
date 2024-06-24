afa0bb109b1c160cca77598a102423245bf67202
Jesse Rodrigues <104332254+Jrodrygues@users.noreply.github.com>
2024-06-24 09:15:17 -0300

    Ultima versao do codigo html css e js

--- script.js
+++ script.js
@@ -37,6 +37,7 @@ $(document).ready(function () {
       }
     }
   ]
+
   var proximoJogoIndex = 0
   var jogo = new Game(novosJogos[proximoJogoIndex].coordenadas)
   var MAX_TENTATIVAS = 3 // Número máximo de tentativas permitidas
@@ -45,6 +46,7 @@ $(document).ready(function () {
   var tempoRestante = TEMPO_LIMITE_SEGUNDOS // Tempo restante inicial
   var timer // ID do temporizador
   var temporizadorIniciado = false // Flag para indicar se o temporizador foi iniciado
+
   // Função para iniciar o próximo jogo
   window.proximoJogo = function () {
     proximoJogoIndex++
@@ -54,17 +56,20 @@ $(document).ready(function () {
     var novoJogo = novosJogos[proximoJogoIndex]
     $('#imagem-esquerda').attr('src', novoJogo.imagens[0])
     $('#imagem-direita').attr('src', novoJogo.imagens[1])
+
     jogo.vars.positions = novoJogo.coordenadas
+
     // Reinicia o jogo
     jogo.start()
     temporizadorIniciado = false // Reseta a flag do temporizador
     tempoRestante = TEMPO_LIMITE_SEGUNDOS // Reinicia o tempo restante
     atualizarTempo() // Atualiza o display do temporizador
-    //$('#timer').text('03:00') // Reseta o temporizador
     // Esconde a mensagem de tempo esgotado, se estiver visível
     hideTimeUpMessage()
+
     // Reinicia o contador de tentativas
     tentativas = 0
+
     // Desabilita o botão "Próximo Jogo" até que todos os erros sejam encontrados novamente
     $('#btn-proximo-jogo').prop('disabled', true)
     // Habilita o botão de jogar novamente
@@ -72,16 +77,17 @@ $(document).ready(function () {
     $('#btn-iniciar').prop('disabled', false) // Habilita o botão de iniciar
     $('#btn-instrucoes').prop('disabled', false) // Habilita o botão de instruções
   }
+
   // Função para reiniciar o jogo
   window.restartGame = function () {
     jogo.start() // Chama o método start() do objeto jogo
-    //temporizadorIniciado = false // Reseta a flag do temporizador
-    //$('#timer').text('03:00') // Reseta o temporizador
+
     // Reseta o temporizador
     clearInterval(timer) // Limpa o temporizador atual, se existir
     temporizadorIniciado = false // Reseta a flag do temporizador
     tempoRestante = TEMPO_LIMITE_SEGUNDOS // Reinicia o tempo restante
     atualizarTempo() // Atualiza o display do temporizador
+
     // Esconde a mensagem de tempo esgotado, se estiver visível
     hideTimeUpMessage()
     // Esconde a mensagem de tempo esgotado, se estiver visível
@@ -90,18 +96,23 @@ $(document).ready(function () {
   function hideTimeUpMessage() {
     $('#time-up-message').fadeOut() // Oculta a mensagem com efeito de fade-out
   }
+
   // Função para iniciar o temporizador
   function iniciarTemporizador() {
     if (!temporizadorIniciado) {
       clearInterval(timer) // Limpa o temporizador anterior, se existir
+
       tempoRestante = TEMPO_LIMITE_SEGUNDOS
       atualizarTempo() // Atualiza o display do tempo inicial
+
       timer = setInterval(function () {
         tempoRestante--
         atualizarTempo()
+
         if (tempoRestante <= 0) {
           clearInterval(timer) // Para o temporizador quando o tempo acabar
           showTimeUpMessage() // Mostra a mensagem de tempo esgotado
+
           // Desabilita outros botões
           $('#btn-jogar-novamente').prop('disabled', false) // Habilita o botão de jogar novamente
           $('#btn-iniciar').prop('disabled', true) // Desabilita o botão de iniciar
@@ -110,23 +121,28 @@ $(document).ready(function () {
           // Lógica adicional de finalização do jogo aqui, se necessário
         }
       }, 1000)
+
       temporizadorIniciado = true // Marca o temporizador como iniciado
     }
   }
+
   // Função para atualizar o display do temporizador
   function atualizarTempo() {
     var minutos = Math.floor(tempoRestante / 60)
     var segundos = tempoRestante % 60
     $('#timer').text(formatTime(minutos) + ':' + formatTime(segundos))
   }
+
   // Função auxiliar para formatar o tempo com zero à esquerda, se necessário
   function formatTime(time) {
     return time < 10 ? '0' + time : time
   }
+
   // Função para mostrar a mensagem de tempo esgotado
   function showTimeUpMessage() {
     $('#time-up-message').fadeIn() // Exibe a mensagem com efeito de fade-in
   }
+
   // Alternar entre modo claro e escuro
   $('#toggle-mode').click(function () {
     $('body').toggleClass('dark-mode')
@@ -136,14 +152,12 @@ $(document).ready(function () {
       $(this).val('Modo Escuro')
     }
   })
-  // Iniciar o temporizador ao clicar no botão "Iniciar"
-  $('#btn-iniciar').click(function () {
-    iniciarTemporizador()
-  })
+
   // Mostrar ou esconder instruções
   $('#btn-instrucoes').click(function () {
-    $('.instructions').toggle()
+    $('#popup').fadeIn() // Exibe o pop-up de instruções
   })
+
   // Função construtora do jogo
   function Game(positions) {
     var $this = this
@@ -224,17 +238,21 @@ $(document).ready(function () {
         return false
       }
     }
+
     $this.verify = function () {
       var distancia, $marker, markerPosition
       var accepts = 0
+
       $canvas_main.find('.marker').each(function (i) {
         $marker = $(this)
         markerPosition = { x: $marker.data('x'), y: $marker.data('y') }
+
         $.each($this.vars.currentPositions, function (index, position) {
           distancia = Math.sqrt(
             Math.pow(markerPosition.x - position.x, 2) +
               Math.pow(markerPosition.y - position.y, 2)
           )
+
           if (distancia < $this.defaults.radius) {
             $marker.addClass('accept')
             delete $this.vars.currentPositions[index]
@@ -243,12 +261,13 @@ $(document).ready(function () {
           }
         })
       })
-      console.log('Número de erros encontrados:', accepts) // Adicionando log de debug
+
       if (accepts === 7) {
         $('#game-message')
           .text('Parabéns! Você detectou todos os riscos!')
           .css('color', 'green')
         clearInterval(timer) // Para o temporizador quando o jogo é ganho
+
         // Habilita o botão "Próximo Jogo"
         $('#btn-proximo-jogo').prop('disabled', false)
         return true
@@ -257,6 +276,7 @@ $(document).ready(function () {
           .text('Tente novamente! Alguns riscos ainda não foram identificados.')
           .css('color', 'red')
         clearInterval(timer) // Para o temporizador quando o jogo é ganho
+
         // Desabilita o botão "Próximo Jogo" se não todos os erros foram encontrados
         $('#btn-proximo-jogo').prop('disabled', true)
         return false
@@ -277,22 +297,27 @@ $(document).ready(function () {
   }
   // Iniciar o jogo
   jogo.start()
+
+  // Mostrar ou esconder instruções
+  $('#btn-instrucoes').click(function () {
+    $('#popup').fadeIn() // Exibe o pop-up de instruções
+  })
+
+  // Fechar o pop-up ao clicar no botão de fechar
+  $('#close-popup, #close-popup-btn').click(function () {
+    $('#popup').fadeOut() // Fecha o pop-up
+  })
+
+  // Fechar o pop-up ao clicar fora do conteúdo
+  $(document).click(function (event) {
+    if (
+      $(event.target).closest('#popup').length === 0 &&
+      $(event.target).attr('id') !== 'btn-instrucoes'
+    ) {
+      $('#popup').fadeOut() // Fecha o pop-up se clicar fora dele e não for o botão de instruções
+    }
+  })
 })
+
 // Função para iniciar o jogo ao carregar a página
 window.proximoJogo() // Inicia o primeiro jogo automaticamente ao carregar a página
-var btnInstrucoes = document.getElementById('btn-instrucoes')
-var instructions = document.querySelector('.instructions')
-btnInstrucoes.addEventListener('click', function () {
-  if (instructions.style.display === 'block') {
-    instructions.style.display = 'none' // Oculta as instruções se estiverem visíveis
-  } else {
-    instructions.style.display = 'block' // Mostra as instruções se estiverem ocultas
-  }
-})
-// Oculta as instruções se clicar em qualquer área fora do menu de instruções
-document.addEventListener('click', function (event) {
-  var target = event.target
-  if (!instructions.contains(target) && target !== btnInstrucoes) {
-    instructions.style.display = 'none'
-  }
-})
